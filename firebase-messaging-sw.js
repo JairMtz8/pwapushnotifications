be00bc7902645480ee0e638cd1231b4090c6919c
Jair Antonio Martínez Valladares <20223tn068@utez.edu.mx>
2025-11-20 13:10:46 -0600

    [Cambios en urls]

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -19,7 +19,6 @@ messaging.onBackgroundMessage((payload) => {
     const title = payload.notification?.title || "Notificación";
     const options = {
         body: payload.notification?.body || "",
-        icon: "/icon-192.png"
     };
     self.registration.showNotification(title, options);
 });
